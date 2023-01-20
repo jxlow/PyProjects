a45f9fbab728eda9d785c6626c42d7b627b7ed5c
JX Low <ljx@pm.me>
2023-01-20 21:06:14 +0800

    inital commit

---
+++ password-generator/password-generator.py
@@ -0,0 +1,15 @@
+import random
+
+def char() :
+    characterlist = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    range = len(characterlist)
+    random_number = random.randrange(0,range)
+    x = characterlist[random_number]
+    return x
+
+password = ""
+for _ in range(15) :
+    password += char()
+    
+finalpassword = password[:5] + '-' + password[5:10]+ '-' + password[10:]
+print(finalpassword)
