0170ce2c6a861cebd60f3640b4d1c75a9cf3320c
JX Low <ljx@pm.me>
2023-02-10 21:21:41 +0800

    initial commit

---
+++ lta_datamall/sample_bus_arrival.py
@@ -0,0 +1,50 @@
+# sample_bus_arrival.py
+
+import requests
+from dotenv import load_dotenv
+import os
+import json
+from datetime import datetime, timedelta
+
+# BS_CODE = int(input("Enter bus stop code: "))
+BS_CODE = 84031
+BUS_SERVICE_NO = 28
+
+load_dotenv()
+url = 'http://datamall2.mytransport.sg/'
+url_path_bus_arrival = 'ltaodataservice/BusArrivalv2?'
+
+url_path_params = {"BusStopCode": BS_CODE}
+
+header = {"AccountKey" : "{}".format(os.environ.get('API_KEY')), 'accept' : 'application/json'}
+
+response = requests.get(url + url_path_bus_arrival + "BusStopCode=" + str(url_path_params['BusStopCode']), headers=header)
+response_dict = json.loads(response.text)
+
+for bus_service in response_dict["Services"] :
+    if bus_service["ServiceNo"] == str(BUS_SERVICE_NO) :
+        # print(bus_service)
+        for k, v in bus_service["NextBus"].items() :
+            if k == "EstimatedArrival" :
+                next_bus_timing = v
+                # print(next_bus_timing)
+
+clean = str(next_bus_timing).split("+")
+next_bus_timing_clean = clean[0]
+next_bus_timing_clean_time = datetime.strptime(next_bus_timing_clean, "%Y-%m-%dT%H:%M:%S")
+
+now = datetime.now()
+now_str = now.strftime("%Y-%m-%dT%H:%M:%S")
+now_str_time = datetime.strptime(now_str, "%Y-%m-%dT%H:%M:%S")
+# print(now_str)
+
+diff = next_bus_timing_clean_time - now_str_time
+# print(diff)
+
+diff_time = str(diff).split(":")
+minutes = diff_time[1]
+
+if int(minutes) < 1 :
+    print("Bus", BUS_SERVICE_NO, "is arriving soon.")
+else :
+    print("Bus", BUS_SERVICE_NO, "is arriving in", minutes, "mins.")
